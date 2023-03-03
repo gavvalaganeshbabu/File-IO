e51d6cead53d2492b303d51f5464cae657d47c26
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-03-03 08:18:34 +0530

    Master Code Added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FIle_IO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reading File using FILE _ IO ");
+        }
+    }
+}
